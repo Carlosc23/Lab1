3555d256d54d90d284dc730605c89664fcd0bab6
Carlos <carlos.calderon097@gmail.com>
2016-07-29 06:39:58 -0600

    Se agrego subrutina para ingreso de datos

--- main.s
+++ main.s
@@ -1,45 +1,34 @@
-@@------------------------------------------------------
-@@Carlos Calderon 15219 
-@@ Ejercicio 1 
-@@ Main: archivo principal que llama subrutinas para calcular el
-@@maximo de 10 numeros
-@@------------------------------------------------------
-.data
+/* ***************************************************************** 
+   main.s
+   Programa que...
+   Autores: 
+   Carlos Calderon ,Carne: 15219
+   Gabriel Brolo ,Carne: 15105
+   Laboratorio1
+   ***************************************************************** */
+
 .align 2
 @@ Variables de inicio 
-@@-----------------------------------------------------------------------------------------------------
+***************************************************************** */
 Bienvenida:.asciz "¡Bienvenid@ al programa! " 
 Instrucciones: .asciz "Este programa calcula el numero entero mayor de 10 que son ingresados por el usuario"
-vector:				.word 0,0,0,0,0,0,0,0,0,0 @@vector de ingreso
-FormatoD:			.asciz "El valor maximo es: %d  \n"
-@@-----------------------------------------------------------------------------------------------------
+***************************************************************** */--
 @@ Codigo de assembler: se coloca en la seccion .text
 	.text
 	.align		2
 	.global		main
 	.type		main,%function
-@@-----------------------------------------------------------------------------------------------------
+ /* ***************************************************************** ***************************************************************** */
 @@ main
 main:
 	@@ Grabar registro de enlace en la pila
 	stmfd	sp!, {lr}
-	@@ Mensaje de inicio
-	ldr r0,=Bienvenida
+	ldr r0,=Bienvenida @@ Mensaje de inicio
 	bl puts
-	@@ Instrucciones
-	ldr r0,=Instrucciones
+	ldr r0,=Instrucciones @@ Instrucciones
 	bl puts
-	@@Subrutina para ingreso
 	ldr r0, =vector
-	bl leer
-	@@Subrutina para calcular maximo
-	ldr r0, =vector
-	bl maximo @@ carga resultado y lo muestra	
-	@@ Despliegue de resultados
-	mov r1, r1
-	ldr r0, =FormatoD
-	bl printf
-	b fin
+	bl leer	@@Subrutina para ingreso
 	
 	fin:
 	mov r3,#0
