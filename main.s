578a5dd90fcbffff5d4480436e7059aab1cc0b79
Carlosc23 <carlos.calderon097@gmail.com>
2016-08-02 01:33:33 -0600

    Versión pre-final, revisar

--- main.s
+++ main.s
@@ -14,12 +14,15 @@
 /***************************************************************** */
 Bienvenida:			.asciz "¡Bienvenid@ al programa! \n" 
 Instrucciones: 		.asciz "Este programa genera numeros aleatorios y le muestra maximos y minimos. \nIngrese la semilla\n"
+nor:				.asciz "Su vector normalizado es:"
 semilla:			.word 0
 formato:			.asciz "%d"
 formato2:			.asciz "Su semilla es: %d\n"
 formato3:			.asciz "Su numero aleatorio es: %d\n"
 formato4:			.asciz "Su decimal es: %f\n"
 formato5:			.asciz "Su maximo es: %f\n"
+formato6:			.asciz "Su minimo es: %f\n"
+formato7:			.asciz "Su promedio del vector normalizado es: %f\n"
 /*vector:				.space 262144*/
 vector:				.space 5
 /***************************************************************** */
@@ -31,17 +34,17 @@ vector:				.space 5
 .global main
 main:
 	/*stmfd   sp!, {lr}*/
-
+	/* Dar bienvenida e instrucciones al usuario*/
 	ldr r0,= Bienvenida
 	bl printf
 	ldr r0,= Instrucciones
 	bl printf
-
+	/*Ingreso de datos*/
 	ldr r8,= semilla
 	ldr r0,= formato
 	mov r1,r8
 	bl scanf
-
+	
 	ldr r2,= semilla
 	ldr r0,= formato2
 	ldr r1,[r2]
@@ -55,61 +58,73 @@ main:
 	ldr r2,[r2]
 	bl lfsr
 
-	ldr r8,=vector
-		mov r6,#0
-		loops:
-			/*cmp r6,#32768*/
-			cmp r6,#5
-			beq continue
-
-			ldr r1,[r8]
-			ldr r0,=formato3
-			bl printf
-			add r8,#4
-			add r6,#1
-			b loops
-	
-continue:
+/* Llamar subrutina para convertir a punto flotante*/
+	/*mov r2,#32768*/
+	mov r2,#5
 	bl convertToFloat
 
-	mov r6,#0
-	mov r8,#0
-	loopr:
-		/*cmp r6,#32768*/
-		cmp r6,#5
-		beq continue2
-
-		ldr r1,=vector
-		add r1,r8
-		vldr s14,[r1]
-		vcvt.f64.f32 d5,s14
-
-		ldr r0,=formato4
-		vmov r2,r3,d5
-		bl printf
-		add r8,#4
-		add r6,#1
-		b loopr
-
-continue2:
+/* Llamar a subrutina para encontrar el minimo del vector*/
 	ldr r0,=vector
-	bl maximo
-	
+	/*mov r1,#32768*/
+	mov r1,#5
+	bl minimo
+/* Imprimir el minimo*/	
 	mov r3,r0
 
-	vldr s14,[r0]
+	vldr s14,[r3]
 	vcvt.f64.f32 d5,s14
+	push {r0-r3}
+	ldr r0,=formato6
+	vmov r2,r3,d5
+	bl printf
+	pop {r0-r3}
+	
+/* Llamar a subrutina para encontrar el maximo del vector*/
+	ldr r0,=vector
+	/*mov r1,#32768*/
+	mov r1,#5
+	bl maximo
+/* Imprimir el maximo*/	
+	mov r3,r0
 
+	vldr s14,[r3]
+	vcvt.f64.f32 d5,s14
+	push {r0-r3}
 	ldr r0,=formato5
 	vmov r2,r3,d5
 	bl printf
+	pop {r0-r3}
+	
 
-	/*bl norm*/
+/*Llamar a subrutina normalizar para el vector*/ 	
+	mov r3,r0
+	bl norm
+/* Llamar a subrutina para encontrar el promedio del vector*/
+	ldr r0,=vector
+	/*mov r1,#32768*/
+	mov r1,#5
+	bl average
+/* Imprimir el promedio*/	
+	mov r3,r0
 
+	vldr s14,[r3]
+	vcvt.f64.f32 d5,s14
+	push {r0-r3}
+	ldr r0,=formato7
+	vmov r2,r3,d5
+	bl printf
+	pop {r0-r3}
+	
+/* Llamar a subrutina imprimir*/
+	/*mov r1,#32768*/
+	ldr r0,=nor
+	bl puts
+	mov r1,#5
+	ldr r0,=vector
+	bl imprimir
+
+/* Finalizar el programa*/
 end:
-	/*mov r3,#0*/
-	/*mov r0,r3*/
-	/*ldmfd  sp!, {lr}*/
-	/*bx lr*/
+		
 	mov r7,#1
 	swi 0
--- random.s
+++ random.s
@@ -20,6 +20,7 @@ vec:				.word 0,0,0,0,0
 .align 2
 .global lfsr,minimo,maximo,average,norm,imprimir, convertToFloat
 
+/* Subrutina para generar numeros aleatorios*/
 lfsr:
 	mov r9,lr /* guarda valor de lr en r9 */
 	mov r6,#0 /* contador:		inicializar en 0 */
@@ -29,7 +30,7 @@ lfsr:
 	/* genera numeros random y los aloja en vector */
 	loop:
 		/*cmp r6,#32768  compara si ya realizo las iteraciones necesarias */
-		cmp r6,#5
+		cmp r6,r1
 		beq retorno
 		
 		movs r2,r2,lsr #1 /* shift a la derecha */
@@ -58,7 +59,7 @@ convertToFloat:
 	mov r8,#0
 
 	loopVer:
-		cmp r6,#5
+		cmp r6,r2
 		beq retornar
 		ldr r1,=vector
 		add r1,r8
@@ -78,17 +79,16 @@ convertToFloat:
 /*Subrutina minimo */
 minimo:
 	push {lr} 
-	@@ Limpiar registros
+	/*Limpiar registros*/
 	and r2,#0
 	and r3,#0
-	@@mov r4,#0
-	@@ Ciclo que calcula el mayor de los numeros	
+	/* Ciclo que calcula el menor de los numeros*/	
 	vldr s2,[r0]
 	ciclo:
-	cmp r3,#5	
+	cmp r3,r1	
 	beq fin
-	add r3,r3,#1  @@ contador
-	vldr s8,[r0] @@ leer en r2 el vector
+	add r3,r3,#1  
+	vldr s8,[r0] 
 	add r0,r0,#4
 	VCMP.F32 S8,S2
 	VMRS APSR_nzcv, FPSCR
@@ -104,17 +104,16 @@ minimo:
 /*Subrutina maximo */
 maximo:
 	push {lr} 
-	@@ Limpiar registros
+	/*Limpiar registros*/
 	and r2,#0
 	and r3,#0
-	@@mov r4,#0
-	@@ Ciclo que calcula el mayor de los numeros	
+	/* Ciclo que calcula el mayor de los numeros*/		
 	vldr s2,[r0]
 	ciclo2:
-	cmp r3,#5	
+	cmp r3,r1	
 	beq fin2
-	add r3,r3,#1  @@ contador
-	vldr s8,[r0] @@ leer en r2 el vector
+	add r3,r3,#1  
+	vldr s8,[r0] 
 	add r0,r0,#4
 	VCMP.F32 S8,S2
 	VMRS APSR_nzcv, FPSCR
@@ -130,16 +129,17 @@ maximo:
 /*Subrutina average */
 average:
 push {lr} 
-	@@ Limpiar registros
+	/*Limpiar registros*/
 	and r2,#0
 	and r3,#0
-	LDR R1, addr_value5		
-	VLDR S22, [R1]	
+	LDR R2, addr_value5		
+	VLDR S22, [R2]	
+	/* Ciclo para calcular el promedio*/
 	ciclo3:
-	cmp r3,#5	
+	cmp r3,r1	
 	beq fin3
-	add r3,r3,#1  @@ contador
-	vldr s8,[r0] @@ leer en r2 el vector
+	add r3,r3,#1  
+	vldr s8,[r0] 
 	add r0,r0,#4
 	VADD.F32 S0,S0,S8
 	b ciclo3
@@ -151,19 +151,20 @@ push {lr}
 /* Subrutina normalizar */
 norm:
 push {lr}
+	/* Limpiar registros*/
 	and r2,#0
 	mov r4,#0
 	ldr r8,=vector
 	vldr s4,[r3]
-	
+	/* Ciclo que normaliza el vector*/	
 	ciclo4:
-	cmp r4,#5	
+	cmp r4,r1	
 	beq fin4
 	add r4,r4,#1
-	vldr s8,[r8] @@ leer en r2 el vector
+	vldr s8,[r8] 
 	VDIV.F32 S6,S8,S4
 	VSTR S6,[R8]
-	add r0,r0,#4
+	add r8,r8,#4
 	
 	b ciclo4
 	
@@ -174,30 +175,33 @@ push {lr}
 /* Subrutina imprimir */
 imprimir:
 	push {lr}
-	and r2,#0
-	and r3,#0
-	mov r4,#0
-	ldr r1,=vector
+	/*Limpiar registros*/
+	mov r6,#0
+	mov r8,#0
+	/*Ciclo para imprimir*/
 	ciclo5:
-	cmp r4,#5
-	beq ciclo5
-	ldr r1,[r1]
-	vmov s15,r1
-	vcvt.f64.f32 d5,s15
-	push {r0}
-	vmov r2,r3,d5
-	ldr r0,=string
-	bl printf
-	pop {r0}
-	add r1,r1,#4
-	b ciclo5
-	fin5:
+		/*cmp r6,#32768*/
+		cmp r6,r1
+		beq retornof
+		ldr r0,=vector
+		add r0,r8
+		vldr s14,[r0]
+		vcvt.f64.f32 d5,s14
+		ldr r0,=formato4
+		vmov r2,r3,d5
+		push {r1}
+		bl printf
+		pop {r1}
+		add r8,#4
+		add r6,#1
+		b ciclo5
+	retornof:	
 	pop {pc}
+	
 /* Variables*/
 addr_value5:
 	.word value5
 	.data
 value5:	.float 5.0
 string:	.asciz "Floating value is: %f\n"
-
-
+formato4:			.asciz "Su decimal es: %f\n"
